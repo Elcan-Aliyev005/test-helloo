80161a60f71a785916b074f704105287518ccba1
The Lord of Darkness <Elcan@div.edu.az>
2025-12-23 13:17:28 +0400

    test

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,11 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: 2002,
     host: true,
   },
   preview: {
-    port: 3000,
+    port: 2002,
     host: true,
   },
 })
